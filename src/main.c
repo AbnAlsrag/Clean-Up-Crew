01f42f9a68411d90e591579faf0829f65b8f69a8
Abn Alsrag <85142438+AbnAlsrag@users.noreply.github.com>
2024-11-27 14:05:06 +0200

    started the idea of making the whole engine in one module:)

--- src/main.c
+++ src/main.c
@@ -3,22 +3,22 @@
 #include "platform/platform.h"
 
 int main(void) {
-    init_render_contex();
+    cuc_engine_init();
 
     vec2f_t player1 = { 100, 100 };
     vec2f_t player2 = { 100, 100 };
     vec2f_t player3 = { 100, 100 };
     // vec2f_t player4 = { 100, 100 };
 
-    renderer_register_splitscreen(&player1, NULL);
-    renderer_register_splitscreen(&player2, NULL);
-    renderer_register_splitscreen(&player3, NULL);
-    // renderer_register_splitscreen(&player4, NULL);
+    cuc_engine_register_splitscreen((void*)&player1, NULL);
+    cuc_engine_register_splitscreen((void*)&player2, NULL);
+    cuc_engine_register_splitscreen((void*)&player3, NULL);
+    // cuc_engine_register_splitscreen(&player4, NULL);
 
     float speed = 5.0f;
 
     while (platform_running()) {
-        renderer_update();
+        cuc_engine_update();
 
         if (platform_is_key_down(PLATFORM_KEYCODE_W)) {
             player1.y -= speed;
@@ -45,6 +45,6 @@ int main(void) {
         }
     }
 
-    deinit_render_contex();
+    cuc_engine_deinit();
     return 0;
 }
